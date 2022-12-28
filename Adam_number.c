459404d792327c903448e69e45d50293df013743
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2022-12-28 20:45:48 +0530

    #include<stdio.h> int main() {     int n,s1,r,s=0,s2,r1,su=0;     scanf("%d",&n);     int t=n;     s1=n*n;     while(t!=0)     {         r=t%10;         t=t/10;         s=s*10+r;     }     s2=s*s;     while(s2!=0)     {         r1=s2%10;         s2=s2/10;         su=su*10+r1;     }     if(su==s1)     printf("True");     else     printf("False"); }

---
+++ Adam_number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,s1,r,s=0,s2,r1,su=0;
+    scanf("%d",&n);
+    int t=n;
+    s1=n*n;
+    while(t!=0)
+    {
+        r=t%10;
+        t=t/10;
+        s=s*10+r;
+    }
+    s2=s*s;
+    while(s2!=0)
+    {
+        r1=s2%10;
+        s2=s2/10;
+        su=su*10+r1;
+    }
+    if(su==s1)
+    printf("True");
+    else
+    printf("False");
+}
