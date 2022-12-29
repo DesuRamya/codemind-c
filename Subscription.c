d98b3b008b9607a375db615485e0d63d0b7c2451
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2022-12-29 18:28:02 +0530

    #include<stdio.h> int main() {     int i,t,x;     scanf("%d",&t);     while(t--)     {         scanf("%d",&x);         if(x>30)         {             printf("YES");         }         else{             printf("NO");         }         printf(" ");     } }

---
+++ Subscription.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,t,x;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d",&x);
+        if(x>30)
+        {
+            printf("YES");
+        }
+        else{
+            printf("NO");
+        }
+        printf("
+");
+    }
+}
