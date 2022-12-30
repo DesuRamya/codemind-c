bd44478ceeae1f45aa5a43b3c76406e2ac244d25
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2022-12-30 20:40:25 +0530

    #include<stdio.h> int main() {     int x,n,i,o,t;     scanf("%d",&t);     while(t--)     {         scanf("%d%d",&x,&n);         o=(x/10)*n;         printf("%d ",o);     } }//placement test

---
+++ Placement_Test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x,n,i,o,t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d",&x,&n);
+        o=(x/10)*n;
+        printf("%d
+",o);
+    }
+}//placement test
