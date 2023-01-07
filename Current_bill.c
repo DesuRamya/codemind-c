828384e8aae462d6463e9eec06a86bda01f7a5d4
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2023-01-07 22:59:32 +0530

    #include<stdio.h> int main() {     float u;     float b,tb,s,c;     scanf("%f",&u);     if(u<200)     {     c=1.20;     }     else if(u>=200&&u<400)     {     c=1.50;     }     else if(u>=400&&u<600)     {     c=1.80;     }     else if(u>=600)     {     c=2.00;     }     b=u*c;     if(b<400)     {     s=100;     }     else     {     s=0.15*b;     }     tb=s+b;     {     printf("%.2f",tb);     }     }

--- Current_bill.c
+++ Current_bill.c
@@ -1,26 +1,36 @@
 #include<stdio.h>
 int main()
 {
-  int u;
-  float c,s,tb,b;
-  scanf("%d",&u);
-  if(u<200)
-  c=1.20;
-  else if(u>=200&&u<400){
-  c=1.50;
-  }
-  else if(u>=400&&u<600){
-  c=1.80;
-  }
-  else if(u>=600){
-  c=2.00;
-  }
-  b=u*c;
-  if(b>=400)
-  s=0.15*b;
-  else if(b<400){
-  s=100;
-  }
-  tb=b+s;
-  printf("%.2f",tb);
-}
+    float u;
+    float b,tb,s,c;
+    scanf("%f",&u);
+    if(u<200)
+    {
+    c=1.20;
+    }
+    else if(u>=200&&u<400)
+    {
+    c=1.50;
+    }
+    else if(u>=400&&u<600)
+    {
+    c=1.80;
+    }
+    else if(u>=600)
+    {
+    c=2.00;
+    }
+    b=u*c;
+    if(b<400)
+    {
+    s=100;
+    }
+    else 
+    {
+    s=0.15*b;
+    }
+    tb=s+b;
+    {
+    printf("%.2f",tb);
+    }
+    }
